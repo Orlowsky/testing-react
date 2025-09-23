755f31732bdf852df66f5b17956759860683ce98
Orlowsky <orlowski.aleksander@gmail.com>
2025-09-23 16:44:15 +0200

    skipniecie komponentu app i dodanie application do app

--- readme.txt
+++ readme.txt
@@ -1,3 +1,17 @@
 moge zaladowac komponent 
 zamockowac dane
 wgrac dane do elementu i jak wyswietli mi sie konkretna odpowiedz to znaczy ze przeszlo
+
+
+co testować ?
+
+check if component render
+czy renderuje z propsami i czy poprawnie renderuje te propsy
+sprawdzenie czy props wyrenderuje nam konkretny tekst lub element
+czy renderuje sie w roznych stanach(logout loigin na menu bar)
+czy reaguje na eventy? buttony swithc inne elementy html itp
+to co sie wyswietli sprawdzic czy sie wyswietliło
+
+
+nie testowac komponentow ktore juz istnieją lub zewnetrznych
+testować żeczy które sa ważne z punktu widzenia usera
---
+++ src/components/Application/Application.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+export const Application = () => {
+  return (
+    <form>
+        <div>
+            <label htmlFor="name">Name</label>
+            <input id="name" name="name" type="text"/>
+        </div>
+        <div>
+            <label htmlFor="job-location">Email</label>
+            <select id="job-location" name="job-location">
+                <option value="">Select a country</option>
+                <option value="US">United States</option>
+                <option value="PL">Poland</option>
+                <option value="DE">Germany</option>
+                <option value="UK">United Kingdom</option>
+                <option value="FR">France</option>
+                <option value="IT">Italy</option>
+                <option value="ES">Spain</option>
+                <option value="Other">Other</option>
+                </select>
+        </div>
+        <div>
+            <label >
+                <input type="checkbox" name="terms" /> I accept the terms and conditions
+            </label>
+        </div>
+        <button>Submit</button>
+    </form>
+  )
+}
