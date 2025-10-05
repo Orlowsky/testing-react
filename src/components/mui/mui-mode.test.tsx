479408aa9ca2c510af8b5393aafafb9a627caaff
Orlowsky <orlowski.aleksander@gmail.com>
2025-10-05 16:59:23 +0200

    custom render, tworzenie test-utils

--- src/components/mui/mui-mode.test.tsx
+++ src/components/mui/mui-mode.test.tsx
@@ -1,14 +1,16 @@
-import {render, screen} from '@testing-library/react';
+import {render, screen} from '../../test-utils';
 import { MuiMode } from './mui-mode';
-import { AppProviders } from '../providers/app-providers';
+/* import { AppProviders } from '../providers/app-providers'; */
 
 describe('MuiMode', () => {
   it('renders the current theme mode', () => {
-    render(<MuiMode />, { 
+    render(<MuiMode /> /* { 
         wrapper: AppProviders
-    });
+    } */);
     const headingElement = screen.getByRole('heading', { level: 1 });
     expect(headingElement).toHaveTextContent('The current theme mode is dark mode');
     })
 
 })
+// custom render function 
+// nie potrzeba wrapera bo jest on w test-utils i tam jest custom render
---
+++ src/test-utils.tsx
@@ -0,0 +1,16 @@
+
+import {render, RenderOptions} from '@testing-library/react'
+import { AppProviders } from './components/providers/app-providers'
+import { ReactElement } from 'react'
+
+
+
+
+const customRender = (ui: ReactElement, options?:Omit<RenderOptions, "wrapper">) =>
+  render(ui, {wrapper: AppProviders, ...options})
+
+// re-export everything, dzieki temu nie trzeba importowac w kompontach gdzie uzywamy custom render
+export * from '@testing-library/react'
+
+// override render method
+export {customRender as render}
